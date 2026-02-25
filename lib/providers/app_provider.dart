11a8e2fd843080f6938d0cbb9a3f2c4860483636
Shaeshae <shripad.mashalkar@consultant.volvo.com>
2026-02-25 15:45:51 +0530

    Update app_provider.dart

--- lib/providers/app_provider.dart
+++ lib/providers/app_provider.dart
@@ -1,29 +1,56 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../data/verses.dart';
+import '../data/verse_model.dart';
+import '../data/verse_repository.dart';
+
+enum LoadState { idle, loading, loaded, error }
 
 class AppProvider extends ChangeNotifier {
   bool _isDarkMode = false;
   Set<int> _favorites = {};
   String _selectedCategory = 'ಎಲ್ಲಾ (All)';
-  int _currentVerseIndex = 0;
+  List<Verse> _allVerses = [];
+  LoadState _loadState = LoadState.idle;
+  String _errorMessage = '';
 
   bool get isDarkMode => _isDarkMode;
   Set<int> get favorites => _favorites;
   String get selectedCategory => _selectedCategory;
-  int get currentVerseIndex => _currentVerseIndex;
+  List<Verse> get allVerses => _allVerses;
+  LoadState get loadState => _loadState;
+  String get errorMessage => _errorMessage;
+
+  List<String> get categories {
+    final chapters = _allVerses.map((v) => v.chapter).toSet().toList();
+    chapters.sort();
+    return ['ಎಲ್ಲಾ (All)', ...chapters];
+  }
 
   List<Verse> get filteredVerses {
-    if (_selectedCategory == 'ಎಲ್ಲಾ (All)') return allVerses;
-    return allVerses.where((v) => v.category == _selectedCategory).toList();
+    if (_selectedCategory == 'ಎಲ್ಲಾ (All)') return _allVerses;
+    return _allVerses.where((v) => v.chapter == _selectedCategory).toList();
   }
 
-  List<Verse> get favoriteVerses {
-    return allVerses.where((v) => _favorites.contains(v.id)).toList();
-  }
+  List<Verse> get favoriteVerses =>
+      _allVerses.where((v) => _favorites.contains(v.id)).toList();
 
   AppProvider() {
     _loadPrefs();
+    loadVerses();
+  }
+
+  Future<void> loadVerses() async {
+    _loadState = LoadState.loading;
+    notifyListeners();
+
+    try {
+      _allVerses = await VerseRepository.fetchAllVerses();
+      _loadState = LoadState.loaded;
+    } catch (e) {
+      _loadState = LoadState.error;
+      _errorMessage = e.toString();
+    }
+    notifyListeners();
   }
 
   Future<void> _loadPrefs() async {
@@ -48,7 +75,8 @@ class AppProvider extends ChangeNotifier {
       _favorites.add(verseId);
     }
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList('favorites', _favorites.map((e) => e.toString()).toList());
+    await prefs.setStringList(
+        'favorites', _favorites.map((e) => e.toString()).toList());
     notifyListeners();
   }
 
@@ -56,12 +84,6 @@ class AppProvider extends ChangeNotifier {
 
   void setCategory(String category) {
     _selectedCategory = category;
-    _currentVerseIndex = 0;
-    notifyListeners();
-  }
-
-  void setCurrentVerseIndex(int index) {
-    _currentVerseIndex = index;
     notifyListeners();
   }
 }
